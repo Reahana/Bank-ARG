7b3a1fac7c8d44556a261a67287fcaca72b63583
Reahana <reahana233@gmail.com>
2023-03-19 14:56:22 +0600

    utilities- all function

---
+++ js/utilities.js
@@ -0,0 +1,15 @@
+function getInputFieldValueById(inputId){
+    const inputField = document.getElementById(inputId)
+    const inputFieldValue = parseFloat(inputField.value);
+    inputField.value ='' ;
+    return inputFieldValue;
+}
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValue = parseFloat(element.innerText);
+    return elementValue;
+}
+function setTextElementValueById(elementId,newValue){
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
