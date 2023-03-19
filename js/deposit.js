7ba94eec0febb9db2b75cc547a55366f214a3363
Reahana <reahana233@gmail.com>
2023-03-19 14:56:36 +0600

    deposite event

--- js/deposit.js
+++ js/deposit.js
@@ -1,18 +1,4 @@
-function getInputFieldValueById(inputId){
-    const inputField = document.getElementById(inputId)
-    const inputFieldValue = parseFloat(inputField.value);
-    inputField.value ='' ;
-    return inputFieldValue;
-}
-function getTextElementValueById(elementId){
-    const element = document.getElementById(elementId);
-    const elementValue = parseFloat(element.innerText);
-    return elementValue;
-}
-function setTextElementValueById(elementId,newValue){
-    const textElement = document.getElementById(elementId);
-    textElement.innerText = newValue;
-}
+
 
 document.getElementById('depositBtn').addEventListener('click',
 function(){
