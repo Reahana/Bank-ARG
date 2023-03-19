a1e0ab054defcd1c9f98989bbe6bbcc3b70d9bb8
Reahana <reahana233@gmail.com>
2023-03-19 15:09:39 +0600

    withdraw file

---
+++ js/withdraw.js
@@ -0,0 +1,12 @@
+document.getElementById('withdawBtn').addEventListener('click',
+function(){
+
+    const newWithdrawAmount = getInputFieldValueById('withdrawAmount');
+    const previousWithdrawTotal= getTextElementValueById('withdrawTotal');
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    setTextElementValueById('withdrawTotal',currentWithdrawTotal);
+
+    const previousBalanceTotal = getTextElementValueById('balanceTotal');
+    const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    setTextElementValueById('balanceTotal',currentBalanceTotal);
+})
